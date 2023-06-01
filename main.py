68b42de96e3092ddfefe47525e0988e7cd9acb85
mattisalan <matti.salan@gmail.com>
2023-06-01 12:57:17 +0300

    Create instace of Bat class for both players and draw them

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import pygame
 from pygame.locals import *
 from constants import *
+from bat import Bat
 
 def main():
     pygame.init()
 
     # Game window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Create game objects
+    player1_bat = Bat(isPlayer1=True)
+    player2_bat = Bat(isPlayer1=False)
+
+    # Sprite groups
+    player_bats = pygame.sprite.Group()
+    player_bats.add(player1_bat, player2_bat)
 
     # GAME LOOP
     while True:
@@ -21,6 +30,8 @@ def main():
 
         # RENDER GRAPHICS
         screen.fill(BG_COLOR)
+
+        player_bats.draw(screen)
 
         pygame.display.flip() # Refresh screen
 
