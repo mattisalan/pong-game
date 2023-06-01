e3022d816773703512af847b00ca2f8946f56b93
mattisalan <matti.salan@gmail.com>
2023-06-01 20:41:01 +0300

    Add functionality to move bats

--- bat.py
+++ bat.py
@@ -1,3 +1,4 @@
+from typing import Any
 import pygame
 from constants import *
 
@@ -7,10 +8,27 @@ class Bat(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
 
         self.image = pygame.Surface([BAT_WIDTH, BAT_HEIGTH])
-        self.image.fill(WHITE)
-
+        self.image.fill(GREY)
         self.rect = self.image.get_rect()
         if isPlayer1:
-            self.rect.center = (2* BAT_WIDTH, SCREEN_HEIGHT / 2)
+            self.rect.center = (BAT_WIDTH, SCREEN_HEIGHT / 2)
         else: 
-            self.rect.center = (SCREEN_WIDTH - 2 * BAT_WIDTH, SCREEN_HEIGHT / 2)
+            self.rect.center = (SCREEN_WIDTH - BAT_WIDTH, SCREEN_HEIGHT / 2)
+
+        self.speed = 0
+
+    def move_up(self):
+        self.speed -= BAT_SPEED
+
+    def move_down(self):
+        self.speed += BAT_SPEED
+
+    def update(self):
+        self.rect.move_ip([0, self.speed])
+
+        # Do not let the bat go out of screen
+        if self.rect.top < 0:
+            self.rect.top = 0
+        elif self.rect.bottom > SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+        
--- main.py
+++ main.py
@@ -27,11 +27,33 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 raise SystemExit
+            
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    player1_bat.move_up()
+                if event.key == pygame.K_s:
+                    player1_bat.move_down()
+                if event.key == pygame.K_UP:
+                    player2_bat.move_up()
+                if event.key == pygame.K_DOWN:
+                    player2_bat.move_down()
+                    
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_w:
+                    player1_bat.move_down()
+                if event.key == pygame.K_s:
+                    player1_bat.move_up()
+                if event.key == pygame.K_UP:
+                    player2_bat.move_down()
+                if event.key == pygame.K_DOWN:
+                    player2_bat.move_up()
+            
 
         # GAME LOGIC
 
         # RENDER GRAPHICS
         screen.fill(BG_COLOR)
+        player_bats.update()
         player_bats.draw(screen)
 
         pygame.display.flip() # Refresh screen
